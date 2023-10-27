d7878766e932422ff98e061c13dc3f98e25ac1a6
JunhoLee92 <97958470+JunhoLee92@users.noreply.github.com>
2023-10-27 23:54:07 +0900

    Lily Update
    릴리 레이저 부분 최종 업데이트
    딜매커니즘 완성
    애니메이션 확인필요

--- Assets/Script/유닛/LilyProjectile.cs
+++ Assets/Script/유닛/LilyProjectile.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LilyProjectile : MonoBehaviour
 {
-
-
+    
     public float poisonDamagePercentage = 0.05f; // 도트 데미지의 백분율
     public float damageInterval = 0.2f; // 데미지 간격
     private bool isDealingDamage = false; // 데미지 적용 중인지 확인하는 플래그
@@ -15,14 +15,18 @@ public class LilyProjectile : MonoBehaviour
     private Vector3 directionToTarget;
     private bool laserCreated = false;  // 레이저가 이미 생성되었는지 확인하는 변수
     public Transform originatingUnitTransform;
-    LilyAttack lilyAttack;
+    internal LilyAttack lilyAttack;
+
     private void Start()
     {
-        //lilyAttack = GetComponent<LilyAttack>();
+     
+        
     }
     private void Update()
     {
-
+        Transform parentTransform = this.transform.parent;
+        LilyAttack parentScript = parentTransform.GetComponent<LilyAttack>();
+        
         //transform.localScale += new Vector3(0.25f, scalingSpeed, 0);
         if (target == null)
         {
@@ -34,17 +38,17 @@ public class LilyProjectile : MonoBehaviour
         {
             CreateLaser();
         }
-        //if (target && originatingUnitTransform)
-        //{
-        //    directionToTarget = (target.transform.position - originatingUnitTransform.position).normalized;
-        //    float distanceToTarget = Vector3.Distance(originatingUnitTransform.position, target.transform.position);
+        if (target && originatingUnitTransform)
+        {
+            directionToTarget = (target.transform.position - originatingUnitTransform.position).normalized;
+            float distanceToTarget = Vector3.Distance(originatingUnitTransform.position, target.transform.position);
 
-        //    // 레이저 스케일 동적 조절
-        //    transform.localScale = new Vector3(0.25f, Mathf.Min(transform.localScale.y + scalingSpeed, distanceToTarget / 10), 0.25f);
+            // 레이저 스케일 동적 조절
+            transform.localScale = new Vector3(0.25f, distanceToTarget / 5, 0.25f);
 
-        //    // 레이저의 시작 위치 동적 조절
-        //    transform.position = originatingUnitTransform.position;
-        //}
+            // 레이저의 시작 위치 동적 조절
+            transform.position = originatingUnitTransform.position;
+        }
 
         //if (Vector2.Distance(transform.position, target.transform.position) <= 0.1f)
         //{
@@ -55,16 +59,16 @@ public class LilyProjectile : MonoBehaviour
     void CreateLaser()
     {
         
-
+        
         directionToTarget = (target.transform.position - transform.position);
         float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
 
         //레이저 스케일 조절
-        transform.localScale = new Vector3(0.25f, distanceToTarget / 10, 0.25f);
+        transform.localScale = new Vector3(0.25f, distanceToTarget/4.5f, 0.25f);
 
         //레이저의 시작 위치를 변경하지 않도록 오프셋 조정
         //transform.position = transform.position + directionToTarget * (distanceToTarget / 2);
-        transform.position = originatingUnitTransform.position;
+        transform.position = this.transform.parent.position;
 
         // 레이저 회전 설정
         transform.rotation = Quaternion.FromToRotation(Vector3.up, directionToTarget);
@@ -89,11 +93,15 @@ public class LilyProjectile : MonoBehaviour
         //{
         //    StartCoroutine(DealDotDamage(monster));  // 도트 데미지 로직 시작
         //}
+        if (other.tag != "Monster") return;
 
-        Debug.Log("LilyProjectile collided with: " + other.gameObject.name);
+        if (isDealingDamage)
+            return;
+        
         IDamageable damageableEntity = other.GetComponent<IDamageable>();
         if (damageableEntity != null && !isDealingDamage)
         {
+            Debug.Log("LilyProjectile collided with: " + other.gameObject.name);
             StartCoroutine(DealDotDamage(damageableEntity));  // DOT 데미지 처리 시작
         }
     }
