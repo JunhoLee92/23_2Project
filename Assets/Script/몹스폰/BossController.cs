9d3cdb1e4613eaa4ad95883c90096e18f8574ebe
JunhoLee92 <97958470+JunhoLee92@users.noreply.github.com>
2023-10-28 00:50:37 +0900

    Boss
    보스 매커니즘 수정
    Hp 분리 / 본진어택후 이동

--- Assets/Script/몹스폰/BossController.cs
+++ Assets/Script/몹스폰/BossController.cs
@@ -9,10 +9,12 @@ public class BossController : MonoBehaviour,IDamageable
     public int AttackPower;
     public bool IsAlive { get; private set; } = true;
     public float maxHealth;
-    public float attackPreparationTime = 30f;
+    public float attackPreparationTime = 5f;
    
 
     private float currentHealth;
+    public float Phase2Hp;
+    public float Phase3Hp;
     private int currentPhase = 1;
     private float phaseHealthLimit;  // 각 페이즈에서 체력 한도
     private float attackTimer;
@@ -37,6 +39,7 @@ public class BossController : MonoBehaviour,IDamageable
         {
             BaseHealth baseHealth = FindObjectOfType<BaseHealth>();
             baseHealth.TakeDamage(AttackPower);  // 50은 보스의 공격력을 나타내는 예시 값입니다.
+            NextPhase();
             attackTimer = attackPreparationTime;  // 타이머 리셋
         }
 
@@ -49,31 +52,28 @@ public class BossController : MonoBehaviour,IDamageable
         if (this == null || gameObject == null)
             return;
         currentHealth -= damage;
-        float healthLost = maxHealth - currentHealth;
+      
 
-        if (healthLost >= 5000 || attackTimer <= 0)
+        if (currentHealth<=0 || attackTimer <= 0)
         {
             // 체력 감소에 따른 페이즈 전환 조건 추가
-            if (currentPhase == 1 && healthLost >= 5000)
+            if (currentPhase == 1 )
             {
                 NextPhase();
+                currentHealth = Phase2Hp;
             }
-            else if (currentPhase == 2 && healthLost >= 10000)
+            else if (currentPhase == 2)
             {
                 NextPhase();
+                currentHealth = Phase3Hp;
             }
-            else if (attackTimer <= 0 && currentPhase < 3) // 마지막 페이즈에서는 attackTimer가 0이되어도 NextPhase를 호출하지 않음
+            else if (currentPhase==3) // 마지막 페이즈에서는 attackTimer가 0이되어도 NextPhase를 호출하지 않음
             {
                 NextPhase();
             }
         }
         Debug.Log("보스현재체력:" + currentHealth);
-        // 보스 체력이 0 이하로 떨어졌을 때 처리
-        if (currentHealth <= 0)
-        {
-            Destroy(gameObject);
-            // 게임 종료 또는 다음 라운드/챕터 로직 추가
-        }
+        
     }
 
     private void NextPhase()
@@ -99,18 +99,24 @@ public class BossController : MonoBehaviour,IDamageable
                 spriteRenderer.flipY = false;
                 transform.rotation = Quaternion.Euler(0, 0, 90); // Rotate to face downward
                 break;
+
+            case 4:
+                Debug.Log("Phase4");
+                break;
         }
 
-        switch (currentPhase)
-        {
-            case 1:break;
-            case 2: Debug.Log("Case2");break;
-            case 3:Debug.Log("Case3");
-                break; ;
-        }
+       
 
         if (currentPhase > 3)
         {
+            if (currentHealth <= 0)
+            {
+                Debug.Log("승리");
+            }
+            else {
+                Debug.Log("패배");
+                    }
+
             // 보스를 세 번 격파했을 때 처리
             Destroy(gameObject);
             // 게임 종료 또는 다음 라운드/챕터 로직 추가
