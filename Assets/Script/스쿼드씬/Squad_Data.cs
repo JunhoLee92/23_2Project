d212158c744618fd428f67f785e8f38dab87e371
JunhoLee92 <97958470+JunhoLee92@users.noreply.github.com>
2023-11-14 12:01:04 +0900

    SquadScene
    스쿼드씬 업데이트

--- Assets/Script/스쿼드씬/Squad_Data.cs
+++ Assets/Script/스쿼드씬/Squad_Data.cs
@@ -1,18 +1,129 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Squad_Data : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Squad_Data Instance { get; private set; }
+
+    public UnitEvolutionData[] unitEvolutionData;
+
+    public List<string> selectedUnitNames = new List<string>(); 
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+  
+
+    //public void ToggleChecked(string unitName)
+    //{
+    //    bool wasChecked = false;
+    //    foreach (var unit in unitEvolutionData)
+    //    {
+    //        if (unit.unitName == unitName)
+    //        {
+    //            wasChecked = unit.isChecked;
+    //            unit.isChecked = !unit.isChecked;
+    //            break;
+    //        }
+    //    }
+
+    //    if (!wasChecked)
+    //    {
+    //        selectedUnitNames.Add(unitName);
+    //    }
+    //    else
+    //    {
+    //        selectedUnitNames.Remove(unitName);
+    //    }
+
+    //    UpdatePrestigeStatus();
+    //}
+
+    public void ToggleChecked(string unitName)
     {
-        
+        bool wasChecked = false;
+        foreach (var unit in unitEvolutionData)
+        {
+            if (unit.unitName == unitName)
+            {
+                wasChecked = unit.isChecked;
+                unit.isChecked = !unit.isChecked;
+                break;
+            }
+        }
+
+        if (!wasChecked)
+        {
+
+            int emptyIndex = selectedUnitNames.IndexOf("");
+            if (emptyIndex != -1)
+            {
+                selectedUnitNames[emptyIndex] = unitName;
+            }
+            else
+            {
+                selectedUnitNames.Add(unitName);
+            }
+        }
+        else
+        {
+            // add empty string 
+            int index = selectedUnitNames.IndexOf(unitName);
+            if (index != -1)
+            {
+                selectedUnitNames[index] = "";
+            }
+        }
+
+        UpdatePrestigeStatus();
+    }
+
+    void UpdatePrestigeStatus()
+    {
+        // reset all unit's isprestige
+        foreach (var unit in unitEvolutionData)
+        {
+            unit.isPrestige = false;
+        }
+
+        // firstclicked unit = prestige unit
+        if (selectedUnitNames.Count > 0)
+        {
+            string leaderName = selectedUnitNames[0];
+            var leaderUnit = Array.Find(unitEvolutionData, u => u.unitName == leaderName);
+            if (leaderUnit != null)
+            {
+                leaderUnit.isPrestige = true;
+            }
+        }
+
+        //Squad_UIManager.Instance.UpdateStandingImage();
+    }
+
+    public void ResetSquadSelection()
+    {
+        foreach (var unit in unitEvolutionData)
+        {
+            unit.isChecked = false;
+            unit.isPrestige = false;
+        }
+        selectedUnitNames.Clear();
+        //Squad_UIManager.Instance.standingImage.sprite = null;
+
+
+        //Squad_UIManager.Instance.UpdateUnitThumbnails();
     }
 }
--- Assets/Script/스쿼드씬/Squad_UIManager.cs
+++ Assets/Script/스쿼드씬/Squad_UIManager.cs
@@ -1,18 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Squad_UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image[] unitThumbnails;
+    public Image standingImage;
+    public Image StandingIdle;
+    public static Squad_UIManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+         
+        }
+        else if (Instance == this)
+        {
+            {
+                Destroy(gameObject);
+            }
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateUnitThumbnails();
+        UpdateStandingImage();
     }
+
+    //void UpdateUnitThumbnails()
+    //{
+    //    for (int i = 0; i < unitThumbnails.Length; i++)
+    //    {
+    //        if (i < Squad_Data.Instance.selectedUnitNames.Count)
+    //        {
+    //            string unitName = Squad_Data.Instance.selectedUnitNames[i];
+    //            UnitEvolutionData unitData = Array.Find(Squad_Data.Instance.unitEvolutionData, u => u.unitName == unitName);
+    //            if (unitData != null)
+    //            {
+    //                unitThumbnails[i].sprite = unitData.thumbnailSprite;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            unitThumbnails[i].sprite = null; // 
+    //        }
+    //    }
+
+    //    if (Squad_Data.Instance.selectedUnitNames.Count > 0)
+    //    {
+    //        string leaderName = Squad_Data.Instance.selectedUnitNames[0];
+    //        var leaderUnit = Array.Find(Squad_Data.Instance.unitEvolutionData, u => u.unitName == leaderName);
+    //        if (leaderUnit != null)
+    //        {
+    //            standingImage.sprite = leaderUnit.standingSprite;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        standingImage.sprite = null;
+    //    }
+    //}
+
+    public void UpdateUnitThumbnails()
+    {
+        if (standingImage == null)
+        {
+            Debug.LogError("StandingImage component is not assigned in Squad_UIManager");
+            return;
+        }
+
+        for (int i = 0; i < unitThumbnails.Length; i++)
+        {
+            if (i < Squad_Data.Instance.selectedUnitNames.Count)
+            {
+                string unitName = Squad_Data.Instance.selectedUnitNames[i];
+                if (!string.IsNullOrEmpty(unitName))
+                {
+                    var unitData = Array.Find(Squad_Data.Instance.unitEvolutionData, u => u.unitName == unitName);
+                    if (unitData != null)
+                    {
+                        unitThumbnails[i].sprite = unitData.thumbnailSprite;
+                    }
+                }
+                else
+                {
+                    unitThumbnails[i].sprite = null; //
+                }
+            }
+            else
+            {
+                unitThumbnails[i].sprite = null; // 
+            }
+
+        }
+
+        //if (Squad_Data.Instance.selectedUnitNames.Count > 0 && !string.IsNullOrEmpty(Squad_Data.Instance.selectedUnitNames[0]))
+        //{
+        //    string leaderName = Squad_Data.Instance.selectedUnitNames[0];
+        //    var leaderUnit = Array.Find(Squad_Data.Instance.unitEvolutionData, u => u.unitName == leaderName);
+        //    if (leaderUnit != null)
+        //    {
+        //        standingImage.sprite = leaderUnit.standingSprite; // 
+        //    }
+        //    else
+        //    {
+        //        standingImage.sprite = null; // Delete Image If there's No Squadleader
+        //    }
+        //}
+    }
+
+    public void UpdateStandingImage()
+    {
+        if (Squad_Data.Instance.selectedUnitNames.Count > 0 && !string.IsNullOrEmpty(Squad_Data.Instance.selectedUnitNames[0]))
+        {
+            string leaderName = Squad_Data.Instance.selectedUnitNames[0];
+            var leaderUnit = Array.Find(Squad_Data.Instance.unitEvolutionData, u => u.unitName == leaderName);
+            if (leaderUnit != null)
+            {
+                standingImage.sprite = leaderUnit.standingSprite; // 
+            }
+            else
+            {
+                Debug.Log(leaderName);
+                standingImage.sprite = StandingIdle.sprite; // Delete Image If there's No Squadleader
+            }
+        }
+    }
+
+   
+
+
+
 }
